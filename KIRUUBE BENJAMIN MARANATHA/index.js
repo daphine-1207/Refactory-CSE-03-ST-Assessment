e1abd4646556c4c091ae116a6848b1bcc1e84392
Benjamin Kiruube Maranatha <benjaminkiruube@gmail.com>
2024-05-17 11:46:39 +0300

    Set up Database

--- KIRUUBE BENJAMIN MARANATHA/index.js
+++ KIRUUBE BENJAMIN MARANATHA/index.js
@@ -1,14 +1,30 @@
 //Dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
 //Instantiations
 const app = express();
 const port = 4001;
 
 //Configuration
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser:false,
+    useUnifiedTopolgy:false,
+});
+mongoose.connection
+.once('open', () => {
+    console.log('Mongoose connected successfully');
+})
+.on('error', err => {
+    console.log(`Error message: ${err, message}`);
+});
 
 //Middleware
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
 
 //Routes
 
