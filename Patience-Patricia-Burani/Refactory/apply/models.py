c9aaa3d09082be0e1642e1466117260a351973ce
Patience Burani <buranipatiencepatricia@gmail.com>
2024-05-17 15:10:31 +0300

    corrected my url

--- Patience-Patricia-Burani/Refactory/apply/models.py
+++ Patience-Patricia-Burani/Refactory/apply/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import datetime
 from django.core.validators import MinLengthValidator
 from django.core.exceptions import ValidationError
 import re
--- Patience-Patricia-Burani/Refactory/apply/urls.py
+++ Patience-Patricia-Burani/Refactory/apply/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_register/', views.add_register, name='add_register'),
+    path('', views.add_register, name='add_register'),
  
 ]
